e73f33f05c45726841497355bc4afdc6d3db7b44
nd <225004051@sastra.ac.in>
2024-04-16 13:51:51 +0530

    first commit

--- render/views.py
+++ render/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'render/index.html', {})
+
+def data_view(request):
+    return render(request, 'render/data.html',{})
