e9e8146f97ca7fe4985658f6b58100385038e47a
nd <225004051@sastra.ac.in>
2024-04-16 14:42:37 +0530

    first c

--- mysite/routing.py
+++ mysite/routing.py
@@ -3,7 +3,7 @@ from django.urls import re_path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-from webs import consumer
+from render import consumer
 from django.core.asgi import get_asgi_application
 
 websocket_urlpatterns = [
--- render/routing.py
+++ render/routing.py
@@ -1,13 +1,8 @@
-import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from mysite import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pro1.settings')
+# routing.py
+from django.urls import path
+from . import consumers
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(
-        routing.websocket_urlpatterns
-    ),
-})
+websocket_urlpatterns = [
+    path('ws/pi/', consumers.PiConsumer.as_asgi()),
+]
