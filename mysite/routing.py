12611302061bd4a483e60b5619d41870cfbc09ad
nidharshni <110120985+HND04@users.noreply.github.com>
2024-04-16 14:59:01 +0530

    Update routing.py

--- mysite/routing.py
+++ mysite/routing.py
@@ -7,7 +7,7 @@ from render import consumer
 from django.core.asgi import get_asgi_application
 
 websocket_urlpatterns = [
-    re_path(r'ws/pi/$', consumer.PiConsumer.as_asgi()),
+    re_path(r'wss/pi/$', consumer.PiConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
