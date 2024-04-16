1b1ad76e36366f0dd6e72dc679b422c2d8411706
nd <225004051@sastra.ac.in>
2024-04-16 14:03:27 +0530

    tempchange

--- render/urls.py
+++ render/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('a', views.index, name='index'),
+    path('', views.data_view, name='data'),
 ]
+
+
+
